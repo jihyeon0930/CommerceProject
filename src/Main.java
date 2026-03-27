cdb20998b648cbbc1124e93d0994a8d357d2f0cb
jihyeon <bjh20049@naver.com>
2026-03-27 16:35:20 +0900

    STEP 1. main 함수에서 Product 클래스 생성후 상품 목록 추가

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -6,11 +8,23 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("[ 실시간 커머스 플랫폼 - 전자제품 ]");
-        System.out.printf("1. %-12s | %12s | %-30s\n", "Galaxy S25", "1,200,000원", "최신 안드로이드 스마트폰");
-        System.out.printf("2. %-12s | %12s | %-30s\n", "iPhone 16", "1,350,000원", "Apple의 초신 스마트폰");
-        System.out.printf("3. %-12s | %12s | %-30s\n", "MacBook Pro", "2,400,000원", "M3 칩셋이 탑재된 노트북");
-        System.out.printf("4. %-12s | %12s | %-30s\n", "irPods Pro", "350,00원", "노이즈 캔슬링 무선 이어폰");
-        System.out.printf("0. %-12s| 프로그램 종료\n", "종료");
+
+        List<Product> products = new ArrayList<>();
+        products.add(new Product("Galaxy S25", 1200000, "최신 안드로이드 스마트폰", 10));
+        products.add(new Product("iPhone 16", 1350000, "Apple의 초신 스마트폰", 20));
+        products.add(new Product("MacBook Pro", 2400000, "M3 칩셋이 탑재된 노트북", 30));
+        products.add(new Product("irPods Pro", 350000, "노이즈 캔슬링 무선 이어폰", 40));
+
+        int index = 1;
+        for (Product p : products) {
+            System.out.printf("%d. %s\t | %,12d원 | %s \t | %d\n",
+                    index++,
+                    p.getName(),
+                    p.getPrice(),
+                    p.getEx(),
+                    p.getStock());
+        }
+        System.out.println("0. 종료\t\t\t | 프로그램 종료");
         int inputNum = sc.nextInt();
         if (inputNum==1) {
             return;
--- src/Product.java
+++ src/Product.java
@@ -1,13 +1,32 @@
 public class Product {
-    //속
+    //속성 (필드)
     private String name;
-    private double price;
+    private int price;
     private String ex;
     private int stock;
 
-    //생
+    //생성자 -> 값 셋팅
+    public Product(String name, int price, String ex, int stock) {
+        this.name = name;
+        this.price = price;
+        this.ex = ex;
+        this.stock = stock;
+    }
 
+    //getter : 생성자로 값 세팅 (setter은 생성 후 값 변경시 필요-재고 수량 변동등..)
+    public String getName() {
+        return name;
+    }
 
+    public int getPrice() {
+        return price;
+    }
 
-    //기
+    public String getEx() {
+        return ex;
+    }
+
+    public int getStock() {
+        return stock;
+    }
 }
