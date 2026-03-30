c5b1557f3eca80f1a1c6766a112ee5aac6b09081
jihyeon <bjh20049@naver.com>
2026-03-30 20:39:22 +0900

    feat: 카테고리 필드 추가

--- src/CommerceSystem.java
+++ src/CommerceSystem.java
@@ -1,34 +1,37 @@
-import java.sql.Array;
 import java.util.ArrayList;
 import java.util.List;
 
-public class CommerceSystem {
-    //속성(필드) - Product 객체들 관리하는 리스트
-    private List<Product> products = new ArrayList<>();
+public class CommerceSystem { //커머스 플랫폼 상품 관리, 사용자 입력 처리
+    //속성(필드) - Product 객체들 관리하는 리스트 -> 카테고리 관리하는 클래스로 변경!!
+    private List<Category> categores = new ArrayList<>();
 
     //생성자
-    public CommerceSystem(List<Product> products) {
-        this.products = products;
+    public CommerceSystem(List<Category> categores) {
+        this.categores = categores;
     }
 
     //상품 리스트 반환 게터
-    public List<Product> getProducts() {
-        return products;
+    public List<Category> getCategores() {
+        return categores;
     }
 
     //기능
     //전체 상품 출력
     public void start() {
         int index = 1;
-        for (Product p : products) {
-            System.out.printf("%d. %s\t | %,12d원 | %s \t | %d\n",
-                    index++,
-                    p.getName(),
-                    p.getPrice(),
-                    p.getEx(),
-                    p.getStock());
+        for (Category c : categores) {
+            System.out.println("[ " + c.getCategoryName() + " ]");
+            for (Product p : c.getProducts()) {
+                System.out.printf("%d. %s\t | %,12d원 | %s \t | %d\n",
+                        index++,
+                        p.getName(),
+                        p.getPrice(),
+                        p.getEx(),
+                        p.getStock());
+            }
+            System.out.println("0. 종료\t\t\t | 프로그램 종료");
         }
-        System.out.println("0. 종료\t\t\t | 프로그램 종료");
     }
+
 
 }
--- src/Main.java
+++ src/Main.java
@@ -8,21 +8,30 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
 
-        System.out.println("[ 실시간 커머스 플랫폼 - 전자제품 ]");
+        System.out.println("[ 실시간 커머스 플랫폼 ]");
 
-        List<Product> products = new ArrayList<>();
-        products.add(new Product("Galaxy S25", 1200000, "최신 안드로이드 스마트폰", 10));
-        products.add(new Product("iPhone 16", 1350000, "Apple의 초신 스마트폰", 20));
-        products.add(new Product("MacBook Pro", 2400000, "M3 칩셋이 탑재된 노트북", 30));
-        products.add(new Product("irPods Pro", 350000, "노이즈 캔슬링 무선 이어폰", 40));
-        //상품 출력
-        CommerceSystem commerceSystem = new CommerceSystem(products); //값 넣어줌
+        //전자제품 상품 리스트 생성
+        List<Product> electronics = new ArrayList<>();
+        electronics.add(new Product("Galaxy S25", 1200000, "최신 안드로이드 스마트폰", 10));
+        electronics.add(new Product("iPhone 16", 1350000, "Apple의 초신 스마트폰", 20));
+        electronics.add(new Product("MacBook Pro", 2400000, "M3 칩셋이 탑재된 노트북", 30));
+        electronics.add(new Product("irPods Pro", 350000, "노이즈 캔슬링 무선 이어폰", 40));
+
+        //Category 객체 생성
+        Category electronicsCategory = new Category("전자제품", electronics);
+
+        //CommerceSystem에 전달 (Category 리스트 관리)
+        List<Category> categores = new ArrayList<>();
+        categores.add(electronicsCategory);
+//        categores.add(clothessCategory); //의류 카테고리 추가
+        CommerceSystem commerceSystem = new CommerceSystem(categores);
         commerceSystem.start();
 
 
         int inputNum = sc.nextInt();
-        if (inputNum==1) {
-            return;
+        if (inputNum==0) {
+            System.out.println("프로그램 종료");
+            System.exit(0);
         }
     }
 }
